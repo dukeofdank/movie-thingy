7a445fbc203075d58400363b2a1f3f8ab79b97ee
dukeofdank <jnbunting9265@gmail.com>
2017-11-24 02:21:56 -0600

    First commit

---
+++ CS4773Assignment4/src/movie/model/Singleton.java
@@ -0,0 +1,60 @@
+package movie.model;
+
+import java.util.Observable;
+
+public class Singleton extends Observable {
+	private static Singleton instance = null;
+	
+	private Movie model;
+	
+	private Singleton() {
+		model = new Movie(null, 0, null, null, 0);
+		System.out.println("Singleton initialized");
+	}
+	
+	public static Singleton getInstance() {
+		if (instance == null)
+			instance = new Singleton();
+		return instance;
+	}
+	
+	public void setMovieTitle(String title) {
+		model.setMovieTitle(title);
+		this.setChanged();
+		this.notifyObservers();
+	}
+	
+	public String getMovieTitle() {
+		return model.getMovieTitle();
+	}
+	
+	public void setReleaseYear(int year) {
+		model.setReleaseYear(year);
+		this.setChanged();
+		this.notifyObservers();
+	}
+	
+	public int getReleaseYear() {
+		return model.getReleaseYear();
+	}
+	
+	public void setDirector(String director) {
+		model.setDirector(director);
+		this.setChanged();
+		this.notifyObservers();
+	}
+	
+	public String getDirector() {
+		return model.getDirector();
+	}
+	
+	public void setWriter(String writer) {
+		model.setWriter(writer);
+		this.setChanged();
+		this.notifyObservers();
+	}
+	
+	public String getWriter() {
+		return model.getWriter();
+	}
+}
---
+++ CS4773Assignment4/src/movie/view/MovieController.java
@@ -0,0 +1,87 @@
+package movie.view;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import movie.model.Singleton;
+import javafx.fxml.Initializable;
+
+public class MovieController implements Initializable, Observer {
+
+    @FXML
+    private TextField movieTitle;
+
+    @FXML
+    private TextField director;
+
+    @FXML
+    private TextField releaseYear;
+
+    @FXML
+    private TextField writer;
+
+    @FXML
+    private Label ratingText;
+
+    @FXML
+    private Slider ratingSlider;
+    
+    private Singleton model;
+
+    public MovieController() {
+    	model = Singleton.getInstance();
+    	model.addObserver(this);
+    }
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		movieTitle.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				model.setMovieTitle(newValue);
+			}			
+		});
+		director.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				model.setDirector(newValue);
+			}			
+		});
+		releaseYear.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				String numeric = newValue;
+				if (!newValue.matches("\\d*")) {
+					numeric = newValue.replaceAll("[^\\d]", "");
+					releaseYear.setText(numeric);
+				}
+				model.setReleaseYear(Integer.valueOf(numeric));
+			}			
+		});
+		writer.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				model.setWriter(newValue);
+			}			
+		});
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		Singleton temp = (Singleton) o;
+		movieTitle.textProperty().setValue(temp.getMovieTitle());
+		director.textProperty().setValue(temp.getDirector());
+		releaseYear.textProperty().setValue(Integer.toString(temp.getReleaseYear()));
+		writer.textProperty().setValue(temp.getWriter());
+	}
+
+}
